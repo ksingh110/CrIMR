febc613868b16942ee183d78817b80c23f5be4ef
ksingh110 <krishaysingh2022@gmail.com>
2025-03-03 09:47:48 -0800

    Delete CSV_To_FASTA.py

--- CSV_To_FASTA.py
+++
@@ -1,5 +0,0 @@
-import os
-output_path = "E:\\cry1controlsequencesencoded.npz"
-output_dir = os.path.dirname(output_path)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir, exist_ok=True)

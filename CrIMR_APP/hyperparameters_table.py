75897a945bae5686f6b5ee5907ffd8803c3b92ce
Krishay Singh <krishaysingh2022@gmail.com>
2025-05-30 00:20:46 -0700

    Update

--- CrIMR_APP/hyperparameters_table.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-import apply_umap_on_data from app.py
-
-model = tf.keras.models.load_model("/Users/krishaysingh/Downloads/6000_5_if_new_best_model.keras")
-model.predict()
--- something.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.framework import ops
-
-# Clear the session to release resources
-tf.keras.backend.clear_session()
-
